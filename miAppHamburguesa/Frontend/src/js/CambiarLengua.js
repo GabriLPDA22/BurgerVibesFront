edc79f467c36059dd694ced5d227759f88762272
Gabriel Saiz <gsaiz.bajo@gmail.com>
2024-04-17 13:12:36 +0200

    Boton de Idioma 2.0

--- miAppHamburguesa/Frontend/src/js/CambiarLengua.js
+++ miAppHamburguesa/Frontend/src/js/CambiarLengua.js
@@ -1,38 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const languageButton = document.getElementById('language-button');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
     const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+    languageButton.addEventListener('click', function() {
+        // Mostrar u ocultar el menú desplegable
+        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    });
 
     dropdownItems.forEach(item => {
         item.addEventListener('click', function (event) {
             event.preventDefault();
             const lang = this.getAttribute('data-lang');
             if (lang === 'en') {
-                translateToEnglish();
+                window.location.href = '/miAppHamburguesa/Frontend/public/en_index.html';
             } else if (lang === 'es') {
-                translateToSpanish();
+                window.location.href = '/miAppHamburguesa/Frontend/public/es_index.html';
             }
+            // Ocultar el menú después de la selección
+            dropdownMenu.style.display = 'none';
         });
     });
 
-    function translateToEnglish() {
-        // Redireccionar a la versión en inglés de la página
-        window.location.href = '../public/en_index.html';
-        addEventListener
-    }
-
-    function translateToSpanish() {
-        // Redireccionar a la versión en español de la página
-        window.location.href = '../public/es_index.html';
-    }
-});
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const languageButton = document.getElementById('language-button');
-    const languageSwitcher = document.getElementById('language-switcher');
-
-    languageButton.addEventListener('click', function() {
-        languageSwitcher.classList.toggle('show');
+    // Cerrar el menú desplegable si se hace clic en cualquier lugar fuera de él
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('#language-button')) {
+            dropdownMenu.style.display = 'none';
+        }
     });
 });
