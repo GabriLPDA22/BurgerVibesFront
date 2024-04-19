5847733ae694912a322615f1ebf5b807e6a8c5de
CoroAlfonso <a27803@svalero.com>
2024-04-19 17:38:55 +0200

    cookies

--- miAppHamburguesa/Frontend/src/js/cookies.js
+++ miAppHamburguesa/Frontend/src/js/cookies.js
@@ -1,11 +1,21 @@
-function acceptCookies() {
-    document.getElementById('cookieConsentContainer').style.display = 'none';
-    localStorage.setItem('cookiesAccepted', 'true');
-  }
-  
-  window.onload = function() {
-    if (localStorage.getItem('cookiesAccepted') !== 'true') {
-      document.getElementById('cookieConsentContainer').style.display = 'block';
-    }
-  }
-  
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+dataLayer = [];
+
+if(!localStorage.getItem('cookies-aceptadas')){
+	avisoCookies.classList.add('activo');
+	fondoAvisoCookies.classList.add('activo');
+} else {
+	dataLayer.push({'event': 'cookies-aceptadas'});
+}
+
+botonAceptarCookies.addEventListener('click', () => {
+	avisoCookies.classList.remove('activo');
+	fondoAvisoCookies.classList.remove('activo');
+
+	localStorage.setItem('cookies-aceptadas', true);
+
+	dataLayer.push({'event': 'cookies-aceptadas'});
+});
