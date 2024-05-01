7dbda42696e5cb493dbfd986fa2aa3a552d89ea7
Gabriel Saiz <gsaiz.bajo@gmail.com>
2024-05-01 19:11:36 +0200

    Arreglo de total que no se hacia bien el pago

--- miAppHamburguesa/Frontend/src/js/LogicaMenu/Pago.js
+++ miAppHamburguesa/Frontend/src/js/LogicaMenu/Pago.js
@@ -7,19 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
         // Confirma que cart es un objeto y tiene la propiedad 'items' que es un array
         if (cart && Array.isArray(cart.items)) {
             renderCartItems(cart.items); // Función para renderizar los artículos del carrito
-            document.getElementById('total-price').textContent = cart.total + ' €'; // Actualiza el precio total
         }
     }
 });
   
 function renderCartItems(items) {
     let itemsContainer = document.querySelector('.my-3');
+    let totalPrice = 0; // Variable para almacenar el precio total del carrito
+
     if (itemsContainer) {
         itemsContainer.innerHTML = ''; // Limpiar el contenido actual
         
         items.forEach(function(item) {
             let orderItem = document.createElement('div');
             orderItem.classList.add('d-flex', 'justify-content-between', 'align-items-center', 'border', 'rounded-3', 'p-3', 'my-3');
+            
+            // Calcular el precio total del artículo (sin multiplicar por la cantidad)
+            totalPrice += item.price; // Sumar el precio del artículo al precio total del carrito
             
             orderItem.innerHTML = `
                 <div class="d-flex align-items-center">
@@ -30,9 +34,10 @@ function renderCartItems(items) {
             
             itemsContainer.appendChild(orderItem); // Agregar el div del artículo al contenedor
         });
+
+        // Actualizar el texto del elemento que muestra el precio total del carrito
+        document.getElementById('total-price').textContent = totalPrice.toFixed(2) + ' €';
     } else {
         console.error("El contenedor de artículos del carrito no se encontró.");
     }
 }
-
-
