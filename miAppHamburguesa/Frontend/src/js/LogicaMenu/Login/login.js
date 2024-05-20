0926c28268233ff5ae9fc6a2a8828bb6d41eb616
Gabriel Saiz <gsaiz.bajo@gmail.com>
2024-05-20 14:32:27 +0200

    arreglos

--- miAppHamburguesa/Frontend/src/js/LogicaMenu/Login/login.js
+++ miAppHamburguesa/Frontend/src/js/LogicaMenu/Login/login.js
@@ -71,46 +71,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    if (registerForm) {
-        registerForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const username = document.getElementById('registerUsername').value;
-            const email = document.getElementById('registerEmail').value;
-            const password = document.getElementById('registerPassword').value;
-
-            try {
-                const response = await fetch('http://localhost:8080/register', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ username, email, password })
-                });
-
-                const responseText = await response.json();
-                if (response.ok) {
-                    alert('Registro exitoso');
-                    localStorage.setItem('authenticated', 'true');
-                    localStorage.setItem('username', responseText.username);
-                    localStorage.setItem('email', responseText.email);
-                    toggleModal(modalRegister, false);
-                    updateUI();
-                } else {
-                    alert('Error en el registro: ' + responseText.message);
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                alert('Error al conectar con el servidor');
-            }
-        });
-    }
-
     if (logoutBtn) {
         logoutBtn.addEventListener('click', (event) => {
             event.preventDefault();
             localStorage.removeItem('authenticated');
             localStorage.removeItem('username');
-            localStorage.removeItem('email');
             updateUI();
         });
     }
@@ -119,68 +84,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const isAuthenticated = localStorage.getItem('authenticated') === 'true';
         if (isAuthenticated) {
             const username = localStorage.getItem('username');
-            const email = localStorage.getItem('email');
-            usernameDisplay.textContent = username;
+            usernameDisplay.innerText = username;
             document.getElementById('userMenu').style.display = 'block';
-            loginBtn.style.display = 'none';
-            registerBtn.style.display = 'none';
-            paymentFormContainer.style.display = 'block';
-            loginMessageContainer.style.display = 'none';
-
-            // Rellenar el formulario de compra con los datos del usuario
-            fullNameInput.value = username;
-            emailInput.value = email;
+            document.querySelector('.login').style.display = 'none';
+            document.querySelector('.register').style.display = 'none';
         } else {
-            usernameDisplay.textContent = '';
+            usernameDisplay.innerText = '';
             document.getElementById('userMenu').style.display = 'none';
-            loginBtn.style.display = 'block';
-            registerBtn.style.display = 'block';
-            paymentFormContainer.style.display = 'none';
-            loginMessageContainer.style.display = 'block';
+            document.querySelector('.login').style.display = 'inline-block';
+            document.querySelector('.register').style.display = 'inline-block';
         }
     }
 
-    // Inicializar la interfaz de usuario
+    // Actualizar la interfaz de usuario al cargar la página
     updateUI();
-
-    // Botón de pago
-    const payButton = document.getElementById('payButton_VAL');
-    if (payButton) {
-        payButton.addEventListener('click', async (event) => {
-            event.preventDefault();
-
-            const paymentDetails = {
-                fullName: fullNameInput.value,
-                phoneNumber: document.getElementById('phone-number').value,
-                email: emailInput.value,
-                pickupTime: document.getElementById('pickup-time').value,
-                restaurantNote: document.getElementById('restaurant-note').value,
-                promoCode: document.getElementById('promo-code').value,
-                cardNumber: document.getElementById('card-number').value,
-                cardExpiry: document.getElementById('card-expiry').value,
-                cardCvc: document.getElementById('card-cvc').value,
-                country: document.getElementById('country').value,
-            };
-
-            try {
-                const response = await fetch('http://localhost:8080/pay', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(paymentDetails)
-                });
-
-                const responseText = await response.json();
-                if (response.ok) {
-                    alert('Pago exitoso');
-                } else {
-                    alert('Error en el pago: ' + responseText.message);
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                alert('Error al conectar con el servidor');
-            }
-        });
-    }
 });
--- miAppHamburguesa/Frontend/src/js/server.js
+++ miAppHamburguesa/Frontend/src/js/server.js
@@ -13,139 +13,141 @@ const dbConfig = {
   connectString: 'burgervibesbbdd.ceotvomboedr.us-east-1.rds.amazonaws.com:1521/orcl'
 };
 
+// Ruta para probar la conexión a la base de datos
 app.get('/test-connection', async (req, res) => {
-    let connection;
-    try {
-      connection = await oracledb.getConnection(dbConfig);
-      res.send('Conexión exitosa a la base de datos Oracle');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error al conectar a la base de datos Oracle');
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error(err);
-        }
+  let connection;
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    res.send('Conexión exitosa a la base de datos Oracle');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al conectar a la base de datos Oracle');
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
       }
     }
-  });
-  
-  app.get('/autocomplete', async (req, res) => {
-    const query = req.query.q;
-    let connection;
-  
-    try {
-      connection = await oracledb.getConnection(dbConfig);
-  
-      const result = await connection.execute(
-        `SELECT NOMBRE, EMAIL, NOMBRE_USUARIO 
-         FROM CLIENTE 
-         WHERE LOWER(NOMBRE) LIKE :query OR LOWER(EMAIL) LIKE :query OR LOWER(NOMBRE_USUARIO) LIKE :query`,
-        { query: `%${query.toLowerCase()}%` }
-      );
-  
-      const users = result.rows.map(row => ({
-        NOMBRE: row[0],
-        EMAIL: row[1],
-        NOMBRE_USUARIO: row[2]
-      }));
-  
-      res.json(users);
-    } catch (err) {
-      console.error("Error en la consulta de autocompletado:", err);
-      res.status(500).send('Error al consultar la base de datos');
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error("Error al cerrar la conexión:", err);
-        }
+  }
+});
+
+// Ruta para autocompletado
+app.get('/autocomplete', async (req, res) => {
+  const query = req.query.q;
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+
+    const result = await connection.execute(
+      `SELECT NOMBRE, EMAIL, NOMBRE_USUARIO 
+       FROM CLIENTE 
+       WHERE LOWER(NOMBRE) LIKE :query OR LOWER(EMAIL) LIKE :query OR LOWER(NOMBRE_USUARIO) LIKE :query`,
+      { query: `%${query.toLowerCase()}%` }
+    );
+
+    const users = result.rows.map(row => ({
+      NOMBRE: row[0],
+      EMAIL: row[1],
+      NOMBRE_USUARIO: row[2]
+    }));
+
+    res.json(users);
+  } catch (err) {
+    console.error("Error en la consulta de autocompletado:", err);
+    res.status(500).send('Error al consultar la base de datos');
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error al cerrar la conexión:", err);
       }
     }
-  });
-  
+  }
+});
+
 // Ruta para registrar un nuevo usuario
 app.post('/register', async (req, res) => {
-    const { username, email, password } = req.body; // Cambiamos para recibir username y email por separado
-    const idCliente = new Date().getTime().toString(); // Generar un ID único basado en el timestamp
-    const fechaRegistro = new Date().toISOString().split('T')[0]; // Obtener la fecha actual en formato YYYY-MM-DD
-    let connection;
-  
-    try {
-      connection = await oracledb.getConnection(dbConfig);
-  
-      // Verificar si el usuario ya existe
-      const result = await connection.execute(
-        `SELECT COUNT(*) AS COUNT FROM CLIENTE WHERE EMAIL = :email OR NOMBRE_USUARIO = :username`,
-        { email, username }
-      );
-  
-      const userExists = result.rows[0][0] > 0;
-  
-      if (userExists) {
-        res.send('Usuario ya existe');
-      } else {
-        // Insertar nuevo usuario
-        await connection.execute(
-          `INSERT INTO CLIENTE (ID_CLIENTE, NOMBRE_USUARIO, EMAIL, CONTRASEÑA, FECHAREGISTRO)
-           VALUES (:idCliente, :username, :email, :password, TO_DATE(:fechaRegistro, 'YYYY-MM-DD'))`,
-          { idCliente, username, email, password, fechaRegistro },
-          { autoCommit: true }
-        );
-        res.send('Registro exitoso');
-      }
-    } catch (err) {
-      console.error("Error al registrar el usuario:", err);
-      res.status(500).send(`Error al registrar el usuario: ${err.message}`);
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error("Error al cerrar la conexión:", err);
-        }
-      }
-    }
-  });
-  
-  // Ruta para iniciar sesión
-  app.post('/login', async (req, res) => {
-    const { usernameEmail, password } = req.body;
-    let connection;
-  
-    try {
-      connection = await oracledb.getConnection(dbConfig);
-  
-      // Verificar las credenciales del usuario
-      const result = await connection.execute(
-        `SELECT NOMBRE_USUARIO, EMAIL FROM CLIENTE WHERE (EMAIL = :usernameEmail OR NOMBRE_USUARIO = :usernameEmail) AND CONTRASEÑA = :password`,
-        { usernameEmail, password }
-      );
-  
-      if (result.rows.length > 0) {
-        const user = result.rows[0];
-        res.send({ message: 'Inicio de sesión exitoso', username: user[0], email: user[1] });
-      } else {
-        res.status(401).send('Credenciales incorrectas');
-      }
-    } catch (err) {
-      console.error("Error al iniciar sesión:", err);
-      res.status(500).send(`Error al iniciar sesión: ${err.message}`);
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          console.error("Error al cerrar la conexión:", err);
-        }
-      }
-    }
-  });
-  
+  const { username, email, password } = req.body; // Cambiamos para recibir username y email por separado
+  const idCliente = new Date().getTime().toString(); // Generar un ID único basado en el timestamp
+  const fechaRegistro = new Date().toISOString().split('T')[0]; // Obtener la fecha actual en formato YYYY-MM-DD
+  let connection;
 
-  app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-  });
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+
+    // Verificar si el usuario ya existe
+    const result = await connection.execute(
+      `SELECT COUNT(*) AS COUNT FROM CLIENTE WHERE EMAIL = :email OR NOMBRE_USUARIO = :username`,
+      { email, username }
+    );
+
+    const userExists = result.rows[0][0] > 0;
+
+    if (userExists) {
+      res.status(400).send({ message: 'El usuario ya existe. Inicia sesión.' });
+    } else {
+      // Insertar nuevo usuario
+      await connection.execute(
+        `INSERT INTO CLIENTE (ID_CLIENTE, NOMBRE_USUARIO, EMAIL, CONTRASEÑA, FECHAREGISTRO)
+         VALUES (:idCliente, :username, :email, :password, TO_DATE(:fechaRegistro, 'YYYY-MM-DD'))`,
+        { idCliente, username, email, password, fechaRegistro },
+        { autoCommit: true }
+      );
+      res.send({ message: 'Registro exitoso', username, email });
+    }
+  } catch (err) {
+    console.error("Error al registrar el usuario:", err);
+    res.status(500).send(`Error al registrar el usuario: ${err.message}`);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error al cerrar la conexión:", err);
+      }
+    }
+  }
+});
+
+// Ruta para iniciar sesión
+app.post('/login', async (req, res) => {
+  const { usernameEmail, password } = req.body;
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+
+    // Verificar las credenciales del usuario
+    const result = await connection.execute(
+      `SELECT NOMBRE_USUARIO, EMAIL FROM CLIENTE WHERE (EMAIL = :usernameEmail OR NOMBRE_USUARIO = :usernameEmail) AND CONTRASEÑA = :password`,
+      { usernameEmail, password }
+    );
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      res.send({ message: 'Inicio de sesión exitoso', username: user[0], email: user[1] });
+    } else {
+      res.status(401).send({ message: 'Credenciales incorrectas' });
+    }
+  } catch (err) {
+    console.error("Error al iniciar sesión:", err);
+    res.status(500).send(`Error al iniciar sesión: ${err.message}`);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error al cerrar la conexión:", err);
+      }
+    }
+  }
+});
+
+// Iniciar el servidor
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
